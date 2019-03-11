560cdc80ba7d8a2355a373f59a44609476f838ee
Birman <Birman.Alexandr@gmail.com>
2019-03-12 02:50:21 +0300

    Lesson 15 FXML anotations

--- src/sample/controllers/MainController.java
+++ src/sample/controllers/MainController.java
@@ -7,6 +7,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -14,8 +17,29 @@ import java.io.IOException;
 
 
 public class MainController {
+
     @FXML
-    private Button addButton;
+    private Button btnAdd;
+
+    @FXML
+    private Button btnChange;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private TextField txtSearch;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private TableView tableAddressBook;
+
+    @FXML
+    private Label labelCount;
+
+
     public void showDialog(ActionEvent actionEvent){
         try {
             Stage stage = new Stage();
