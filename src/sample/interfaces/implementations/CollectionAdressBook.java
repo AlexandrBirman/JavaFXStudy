910216cd1d465bb922dc97969367fa6ebc8291a3
AlexandrBirman <birman.alexandr@gmail.com>
2019-04-25 22:56:35 +0300

    Lesson 17 interface implementation

--- src/sample/interfaces/implementations/CollectionAdressBook.java
+++ src/sample/interfaces/implementations/CollectionAdressBook.java
@@ -29,4 +29,23 @@ public class CollectionAdressBook implements AdressBook{
         personList.remove(person);
     }
 
+    public void print(){
+        int number = 0;
+        System.out.println();
+        for(Person person : personList){
+            number++;
+            System.out.println(number+") fio = "+person.getFio()+"; phone = "+person.getPhone());
+        }
+    }
+
+    public void fillTestData(){
+        personList.add(new Person("Иван Печкин", "23948723948"));
+        personList.add(new Person("Роман Романов", "345345345"));
+        personList.add(new Person("Антон Иванов", "345345345"));
+        personList.add(new Person("Джон Маклейн", "23423423"));
+        personList.add(new Person("Джек Воробей", "234234"));
+        personList.add(new Person("Алиса Ивановна", "456456"));
+        personList.add(new Person("Боб Марли", "34534345"));
+    }
+
 }
